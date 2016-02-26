c45f3bb5b513c298d2cca3ecbaa7f7ca58744842
Kimiko Kano <kskano@gmail.com>
2016-02-25 22:22:06 -0700

    add uniqueness validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:username) }
+  context 'validations' do
+    it { should validate_presence_of(:username) }
+
+    it do
+      should validate_uniqueness_of(:username).
+      case_insensitive
+    end
+  end
 end
